7161d8be0fab33387f9c8566528cbf3318c4568c
Morgan Roux <roux.morgan@gmail.com>
2020-01-17 12:20:05 +0100

    day04 ex01 advanced

---
+++ day04/ex01/YoungestFellah.py
@@ -0,0 +1,20 @@
+from FileLoader import FileLoader
+import pandas as ps
+
+def youngestFellah(df, year):
+	fem = df.loc[	
+		(df['Sex'] == 'F') & (df['Year'] == year) & (df['Medal'] == 'Gold') , 
+		:
+	]
+	mal = df.loc[	
+		(df['Sex'] == 'M') & (df['Year'] == year) & (df['Medal'] == 'Gold') , 
+		:
+	]
+
+	#print(fem.loc[ fem['Age'] == fem['Age'].min(), :])
+	#print(mal.loc[ mal['Age'] == mal['Age'].min(), :])
+	return { 'f' : fem['Age'].min(), 'm' : mal['Age'].min()}
+
+loader = FileLoader()
+df = loader.load("./resources/athlete_events.csv")
+print(youngestFellah(df, 2004))
